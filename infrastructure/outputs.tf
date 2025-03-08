e95640100894a88be391c60937c5b0f2be1cbc10
Anton Aks <antonaks84@gmail.com>
2025-03-08 09:02:14 +0100

    update outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -10,9 +10,19 @@ output "liga_parser_lambda_arn" {
   value       = aws_lambda_function.liga_parser.arn
 }
 
+output "headline_collector_lambda_arn" {
+  description = "ARN of the Liga News Parser Lambda function"
+  value       = aws_lambda_function.headline_collector.arn
+}
+
 output "news_data_bucket_name" {
   description = "Name of the S3 bucket for news data"
   value       = aws_s3_bucket.news_data_bucket.bucket
+}
+
+output "headlines_data_bucket_name" {
+  description = "Name of the S3 bucket for news data"
+  value       = aws_s3_bucket.headlines_data_bucket.bucket
 }
 
 output "lambda_layer_arn" {
